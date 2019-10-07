471db6c15692d64e26088c675c67a67e2bedcca3
Toleutai Bekzat <toleutaib@gmail.com>
2019-10-07 20:57:58 +0600

    Задание 2: прочитал все конструкторы класса List<>

--- Reflection_HW/Reflection_HW/Program.cs
+++ Reflection_HW/Reflection_HW/Program.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Reflection_HW
 {
   class Program
   {
+
     static void Main(string[] args)
+    {
+      Console.WriteLine("Task1: ");
+      DemoTask1();
+      Console.WriteLine("\n\n\n\nTask 2:");
+      DemoTask2();
+      Console.ReadKey();
+    }
+
+    private static void DemoTask2()
+    {
+      Console.WriteLine("Список конструктора класса List<>:");
+
+      var constructorsOfList = typeof(List<>).GetConstructors();
+
+      foreach (var item in constructorsOfList)
+      {
+        Console.WriteLine(item);
+      }
+      Console.ReadLine();
+    }
+
+    static void DemoTask1()
     {
       Console.WriteLine("Введите текст:");
       string text = Console.ReadLine();
@@ -12,12 +37,15 @@ namespace Reflection_HW
       int sizeSubstring;
       Console.WriteLine("Введите размер для Substring: ");
       string number = Console.ReadLine();
-      Int32.TryParse(number, out sizeSubstring);
+      bool isTrue = int.TryParse(number, out sizeSubstring);
 
-      var afterSubstring = text.GetType().GetMethod("Method: Substring", new[] { typeof(int) }).Invoke(text, new object[] { sizeSubstring });
+      object afterSubstring = null;
+      if (isTrue)
+      {
+        afterSubstring = text.GetType().GetMethod("Substring", new Type[] { typeof(int) }).Invoke(text, new object[] { sizeSubstring });
+      }
 
       Console.WriteLine(afterSubstring);
-      Console.ReadKey();
     }
   }
 }
