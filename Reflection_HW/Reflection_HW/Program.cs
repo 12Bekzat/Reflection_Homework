e5fb7e9eaf1619bd37c99e0f1132b238cf8e2ee8
Toleutai Bekzat <toleutaib@gmail.com>
2019-10-07 20:46:15 +0600

    Задание 1: попросил ввести размер для Substring и вывел результат

---
+++ Reflection_HW/Reflection_HW/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reflection_HW
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите текст:");
+      string text = Console.ReadLine();
+
+      int sizeSubstring;
+      Console.WriteLine("Введите размер для Substring: ");
+      string number = Console.ReadLine();
+      Int32.TryParse(number, out sizeSubstring);
+
+      var afterSubstring = text.GetType().GetMethod("Method: Substring", new[] { typeof(int) }).Invoke(text, new object[] { sizeSubstring });
+
+      Console.WriteLine(afterSubstring);
+      Console.ReadKey();
+    }
+  }
+}
